06521e8f720bf129d65d8b51fcaacb2ec81bac55
lodity <lodity9@gmail.com>
2023-09-29 16:39:14 +0300

    Added ability to move nodes

--- Controls/ControlNode.xaml.cs
+++ Controls/ControlNode.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CDM_Lab_3._1.Controls
 {
@@ -20,9 +10,52 @@ namespace CDM_Lab_3._1.Controls
     /// </summary>
     public partial class ControlNode : UserControl
     {
-        public ControlNode()
+        private Point CurrentPos;
+        private Point CurrentMousePosition;
+        public int index;
+        public ControlNode(int index, Point point)
         {
             InitializeComponent();
+            Panel.SetZIndex(this, 2);
+            this.index = index;
+            Text.Text = $"x{index}";
+            RenderTransform = new TranslateTransform();
+            (RenderTransform as TranslateTransform).X = point.X;
+            (RenderTransform as TranslateTransform).Y = point.Y;
+        }
+        public Point Position
+        {
+            get
+            {
+                Point res = new Point();
+                res = new Point((RenderTransform as TranslateTransform).X, (RenderTransform as TranslateTransform).Y);
+                return res;
+            }
+        }
+        private void NodeBorder_MouseMove(object sender, MouseEventArgs e)
+        {
+            Vector diff = e.GetPosition(Parent as Window) - CurrentMousePosition;
+            if (NodeBorder.IsMouseCaptured)
+            {
+                (RenderTransform as TranslateTransform).X += diff.X;
+                (RenderTransform as TranslateTransform).Y += diff.Y;
+                CurrentPos.X = (RenderTransform as TranslateTransform).X;
+                CurrentPos.Y = (RenderTransform as TranslateTransform).Y;
+                CurrentMousePosition = e.GetPosition(Parent as Window);
+                // Runtime updating arcs
+                //Moved?.Invoke(sender, e);
+            }
+        }
+
+        private void NodeBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CurrentMousePosition = e.GetPosition(Parent as Window);
+            NodeBorder.CaptureMouse();
+        }
+        private void NodeBorder_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (NodeBorder.IsMouseCaptured)
+                NodeBorder.ReleaseMouseCapture();
         }
     }
 }
