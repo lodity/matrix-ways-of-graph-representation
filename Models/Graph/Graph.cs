0770842646d3dfc0a0dda3c6bcca38b63be684fe
lodity <lodity9@gmail.com>
2023-10-26 11:59:40 +0300

    Added RemoveNode method

--- Models/Graph/Graph.cs
+++ Models/Graph/Graph.cs
@@ -24,6 +24,25 @@ namespace CDM_Lab_3._1.Models.Graph
             Node node = new(Nodes.Count);
             Nodes.Add(node);
         }
+        public void RemoveNode(int nodeIndex)
+        {
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                for (int j = 0; j < Nodes[i].Children.Count; j++)
+                {
+                    if (Nodes[i].Children[j].Item1 == nodeIndex)
+                        Nodes[i].Children.RemoveAt(j);
+                }
+            }
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                if (Nodes[i].Id == nodeIndex)
+                {
+                    Nodes.RemoveAt(i);
+                    break;
+                }
+            }
+        }
         public IEnumerator GetEnumerator()
         {
             return Nodes.GetEnumerator();
